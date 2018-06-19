8236a3a67c37593127b2d7ffe61e89afe42fce0e
Bohdan Rybchynskiy <web.dev.asoft@gmail.com>
2018-06-19 12:14:30 +0300

    merge with dev

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -5,19 +5,11 @@
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-<<<<<<< HEAD
                     <h2>Category Management</h2>
                 </div>
                 <div class="pull-right">
                     <a class="btn btn-success" href="{{ route('categories.create') }}"> Create New Category</a>
-=======
-                    <h2>Products</h2>
-                </div>
-                <div class="pull-right">
-                    @can('posts-create')
-                        <a class="btn btn-success" href="{{ route('categories.create') }}"> Create New Product</a>
-                    @endcan
->>>>>>> 4d17b3e870af28adaa2c48cc0897cf6dbda1c51d
+
                 </div>
             </div>
         </div>
@@ -33,12 +25,9 @@
         <table class="table table-bordered">
             <tr>
                 <th>No</th>
-<<<<<<< HEAD
-                <th>Name</th>
-=======
+
                 <th>Title</th>
                 <th>Status</th>
->>>>>>> 4d17b3e870af28adaa2c48cc0897cf6dbda1c51d
                 <th width="280px">Action</th>
             </tr>
             @foreach ($categories as $category)
@@ -46,13 +35,6 @@
                     <td>{{ ++$i }}</td>
                     <td>{{ $category->name }}</td>
                     <td>
-<<<<<<< HEAD
-                        <a class="btn btn-info" href="{{ route('categories.show',$category->id) }}">Show</a>
-                        <a class="btn btn-primary" href="{{ route('categories.edit',$category->id) }}">Edit</a>
-                        {!! Form::open(['method' => 'DELETE','route' => ['categories.destroy', $category->id],'style'=>'display:inline']) !!}
-                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                        {!! Form::close() !!}
-=======
                         <label class="badge
                                 @if($category->status == 'processing')
                             badge-warning
@@ -75,16 +57,14 @@
                             <button type="submit" class="btn btn-danger">Delete</button>
                             {{--@endcan--}}
                         </form>
->>>>>>> 4d17b3e870af28adaa2c48cc0897cf6dbda1c51d
+
                     </td>
                 </tr>
             @endforeach
         </table>
-<<<<<<< HEAD
-=======
+
 
 
         {!! $categories->links() !!}
->>>>>>> 4d17b3e870af28adaa2c48cc0897cf6dbda1c51d
     </div>
 @endsection
