b6bd3d54c96d980d60922874414ff4d2b1ff3fd6
Bohdan Rybchynskiy <web.dev.asoft@gmail.com>
2018-06-19 12:10:12 +0300

    little change routes

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,8 @@ Route::prefix('blog')->group(function () {
     Route::get('/{alias}', 'BlogController@index')->name('blog.category');
 });
 
-Route::group(['middleware' => ['role:Admin','auth']], function () {
-    Route::namespace('Admin')->group(function () {
-        Route::get('admin-panel', 'AdminController@index')->middleware([''])->name('admin.dashboard');
+Route::group(['middleware' => ['role:Admin'],'namespace'=>'Admin'], function () {
+        Route::get('admin-panel', 'AdminController@index')->name('admin.dashboard');
         Route::prefix('admin-panel')->group(function () {
             Route::resource('roles', 'RoleController');
             Route::resource('users', 'UserController');
@@ -22,5 +21,4 @@ Route::group(['middleware' => ['role:Admin','auth']], function () {
             Route::resource('categories', 'CategoryController');
             Route::resource('perms', 'PermissionController')->except(['edit', 'update']);
         });
-    });
 });
